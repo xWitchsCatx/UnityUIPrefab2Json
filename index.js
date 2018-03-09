cec10f523b6f1b80b19c3fb610efedbeb138cb94
Dinh Ngoc Minh <minh.dinhngoc@gameloft.com>
2018-03-09 16:46:50 +0700

    +Add first version support button and image

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 const os = require('os');
 
+//get file
 let targetFile = null;
+process.argv = [
+    '',
+    '-in',
+    './testData/Canvas.prefab'
+];
+
 const len = process.argv.length;
 
 for (let i = 0; i < len; i++) {
@@ -16,50 +23,172 @@ if(targetFile === null)
     return process.exit(0);
 }
 
-function readNextObject(lines, index) {
-    let rs = {
-        obj : undefined,
-        nextIndex : index,
-    };
+//=====================================================
 
-    const length = lines.length;
-    while(index < length) {
-        let line = lines[index];
-        //too short ?
-        if(line.length < 3) {
-            index++;
-            continue;
-        }
-        //
-        if(line.startsWith('---')) {
-
+const regExStartObj = /(^-{3})([0-9a-zA-Z\!\s]*)&([0-9]+)/i;
+const regExProp = /([0-9a-zA-Z_]+):\s([:{}0-9a-zA-Z\s,.-_]+)/i;
+const parseXY = (x) => {
+    const regRs = x.match(/x:\s([0-9.-]+),\sy:\s([0-9.-]+)/i);
+    if (regRs) {
+        return {
+            x: parseInt(regRs[1]),
+            y: parseInt(regRs[2]),
         }
     }
+    else {
+        console.log("Can't parse XY:" + x);
+        return '';
+    }
+};
+
+const blacklistProp = [
+    'Prefab',
+    'MonoBehaviour',
+    'CanvasRenderer',
+    'Canvas',
+];
+const whitelistProp = {
+    m_Name : x => x,
+    m_AnchorMin : parseXY,
+    m_AnchorMax : parseXY,
+    m_AnchoredPosition : parseXY,
+    m_SizeDelta : parseXY,
+    m_Pivot : parseXY,
+    m_GameObject : x => x.match(/fileID:\s(\d+)/i)[1],
+};
+const supportElement = [
+    'BTN',
+    'IMG',
+];
+
+function readNextObject(lines, index) {
+    const length = lines.length;
+    let i = index;
+    while(i < length) {
+        const line = lines[i];
+        //start of object ?
+        const regRs = line.match(regExStartObj);
+        if(regRs) {
+            const rs = readObject(lines, i, regRs[3]);
+            if(rs) {
+                return rs;
+            }
+        }        
+        i++;        
+    }
+    return undefined;
 }
 
-function readObject(lines, index) {
-
-}
-
-// fs.readFile(targetFile, (err, data) => {
-//     if(err != null) {
-//         return console.log('Read file Error', err);
-//     }
+function readObject(lines, index, id) {
+    let i = index;
+    let rs = {
+        obj : {
+            id,
+        },        
+        nextIndex : i,
+    };
     
-//     const lines = data.toString().split(os.EOL);
-//     const length = lines.length;
-//     let objs = [];
-//     let index = 0;
+    //read type
+    i++;
+    let line = lines[i].trim();
+    line = line.substr(0, line.length - 1);
+    if(blacklistProp.some(x => line === x)) {
+        return undefined;
+    } else {
+        rs.obj["type"] = line;
+    }
 
-//     while(index < length) {
-//         let newObj = readNextObject(lines, index);
-//         index = newObj.nextIndex;
-//         if(newObj.obj != undefined) {
-//             objs.push(newObj.obj);
-//         }
-//     }
+    const length = lines.length;
+    while(i < length) {
+        line = lines[i].trim();
 
-// });
+        //match object
+        let regRs = line.match(regExStartObj);
+        if(regRs) {
+            rs.nextIndex = i;
+            return rs;
+        }
+
+        //read prop
+        regRs = line.match(regExProp);
+        if(regRs) {            
+            //white list
+            if(Object.prototype.hasOwnProperty.call(whitelistProp, regRs[1])) {
+                rs.obj[regRs[1]] = whitelistProp[regRs[1]](regRs[2]);
+            }
+        }
+        i++;
+    }
+    rs.nextIndex = i;
+    return rs;
+}
+
+fs.readFile(targetFile, (err, data) => {
+    if(err != null) {
+        return console.log('Read file Error', err);
+    }
+    console.log('Read file successful');
+    const lines = data.toString().split('\n');
+    let length = lines.length;
+    let objs = [];
+    let index = 0;
+    let rs = readNextObject(lines, index);
+    while(rs) {
+        objs.push(rs.obj);
+        index = rs.nextIndex;
+        rs = readNextObject(lines, index);
+    }
+
+    //filter object
+    objs = objs.filter((x) => {
+        if(Object.prototype.hasOwnProperty.call(x, 'm_Name')) {
+            const split = x['m_Name'].split('_');
+            if(split.length < 2) {
+                return false;
+            }
+
+            if(supportElement.some( x => x === split[0])) {
+                x['m_Name'] = split[1];
+                x['uiType'] = split[0];
+                return true;
+            }
+
+            return false;
+        } else {
+            return true;
+        }
+    });
+
+    //find rect transform for gameobject
+    length = objs.length;
+    for(let i = 0; i < length; ++i) {
+        let obj = objs[i];
+        if(obj.type === 'GameObject') {
+            //find its rect transform from end to start
+            for(let j = length - 1; j >= 0; --j) {
+                let rectTrans = objs[j];
+                if(rectTrans.type === 'RectTransform') {
+                    if(rectTrans.m_GameObject === obj.id) {
+                        //assign data to gameObject
+                        obj.m_AnchorMin = rectTrans.m_AnchorMin;
+                        obj.m_AnchorMax = rectTrans.m_AnchorMax;
+                        obj.m_AnchoredPosition = rectTrans.m_AnchoredPosition;
+                        obj.m_SizeDelta = rectTrans.m_SizeDelta;
+                        obj.m_Pivot = rectTrans.m_Pivot;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    //filter gameobject
+    objs = objs.filter(x => x.type === 'GameObject');
+
+    fs.writeFileSync('./out.json', JSON.stringify(objs))
+
+    process.exit(1);
+});
 
 
 
@@ -72,4 +201,3 @@ function readObject(lines, index) {
 // console.log(JSON.parse('[{"a":1,"b":2,"c":3},2]'));
 
 // fs.writeFileSync('./out.json', JSON.stringify(obj))
-console.log('--- !u!1001 &100100000'.match(/[^-]{3}&([0-9]+)/i)[1]);
